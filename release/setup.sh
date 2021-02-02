4ab549b090ddebdba133c4522360e76994f0ecbc
chencen2000 <34563412+chencen2000@users.noreply.github.com>
2021-02-02 14:22:25 -0800

    Update setup.sh

--- release/setup.sh
+++ release/setup.sh
@@ -1,45 +1,25 @@
 #!/bin/bash -xv
+
 set -e
-
-# if [[ $EUID -ne 0 ]]; then
-#    echo "This script must be run as root"
-#    exit 1
-# fi
-
-# echo "This script is running as root $SUDO_USER"
 
 if [[ $(lsb_release -rs) != "20.04" ]]; then
    echo "Non-compatible version"
    exit 2
 fi
 
-# strwho=$(who)
-# #echo $string
-# curuser=(`echo $strwho | tr ':' ' '` )  
-# echo $curuser
+# if [[ $(id -u) -ne 0 ]] ; then 
+#     echo "Please run as root" 
+#     exit 1 
+# fi
 
-
+# create folder
 sudo mkdir -p /opt/futuredial
 sudo chown $USER:$USER /opt/futuredial
-sudo mkdir -p /opt/futuredial/athena
+sudo mkdir -p /opt/futuredial/athena.release
 sudo mkdir -p /opt/futuredial/hydradownloader
-sudo chown $USER:$USER /opt/futuredial/athena
+sudo chown $USER:$USER /opt/futuredial/athena.release
 sudo chown $USER:$USER /opt/futuredial/hydradownloader
-#chown athena:athena /opt/athena
-#chown $SUDO_USER:$SUDO_USER /opt/athena
-
-# echo add environment
-if [[ -z $ATHENAHOME ]]; then 
-   echo "set ATHENAHOME=/opt/futuredial/athena"
-   export ATHENAHOME=/opt/futuredial/athena
-   # echo 'export ATHENAHOME=/opt/futuredial/athena' >> /home/$curuser/.bashrc
-   # source /home/$curuser/.bashrc
-   echo 'export ATHENAHOME=/opt/futuredial/athena' >> ~/.bashrc
-   source ~/.bashrc
-fi
-
-echo $ATHENAHOME
-sudo apt install ssh redis -y
+ATHENAHOME=/opt/futuredial/athena.release
 
 # echo "input serial number"
 # echo -e "\e[1;31mThis is red text\e[0m"
@@ -53,25 +33,13 @@ while read -r -n 1 key; do
    # Add the key to the variable which is pressed by the user.
    serialnumber+=$key
 done
-echo $serialnumber
+echo 'Install Athena using serial number: ' $serialnumber
 
-echo "start downloading anthenacmc"
-wget https://github.com/zytzjx/anthenacmc/raw/master/anthenacmc -O anthenacmc
-
-cp ./anthenacmc $ATHENAHOME/anthenacmc
-chmod +x $ATHENAHOME/anthenacmc
-
-# $ATHENAHOME/anthenacmc -uuid=$serialnumber
-# if [ $? -eq 0 ]; then
-#   echo "Success: Serial Number is verified."
-# else
-#   echo "Failure: Serial Number can not be verify." >&2
-#   exit 3
-# fi
-
+sudo apt-get update
 sudo apt install python3-pip python3-opencv python3-opencv-apps openjdk-11-jre-headless -y
 sudo apt install gphoto2 libgphoto2-dev qt5-default python3-zbar python3-serial zip unzip -y
 sudo apt install nvidia-cuda-toolkit -y
+
 sudo pip3 install gphoto2
 sudo pip3 install redis
 sudo pip3 install pyqt5 pyqtchart
@@ -81,59 +49,22 @@ sudo pip3 install pyzbar exif
 sudo pip3 install python-stdnum JPype1 pyro4 psutil
 sudo pip3 install tensorflow==2.3.0
 
-# sudo usermod -a -G dialout $curuser
-# sudo usermod -a -G tty $curuser
-
-sudo usermod -a -G dialout $USER
-sudo usermod -a -G tty $USER
-
-# pip3 install gphoto2
-# pip3 install redis
-# pip3 install pyqt5 pyqtchart
-# pip3 install pyserial
-# pip3 install willow imutils pandas scikit-image
-# pip3 install pyzbar
-# pip3 install python-stdnum JPype1
-
-# echo "Need reboot after finish."
-#reboot
-
-#download 
-echo "start downloading the CMC config..."
+# cd /opt/futuredial/athena.release
 cd $ATHENAHOME
-$ATHENAHOME/anthenacmc -uuid=$serialnumber
+
+echo "start downloading CMC tool"
+wget https://github.com/zytzjx/anthenacmc/raw/master/anthenacmc -O anthenacmc
+chmod +x anthenacmc
+./anthenacmc -uuid=$serialnumber
 if [ $? -eq 0 ]; then
   echo "Success: Serial Number is verified."
 else
   echo "Failure: Serial Number can not be verify." >&2
   exit 3
 fi
-
-echo "start downloading hydradownload"
+echo "start downloading CMC downloader"
 wget https://github.com/zytzjx/hydradownload/raw/master/hydradownload -O hydradownload
 chmod +x hydradownload
 
-wget https://raw.githubusercontent.com/zytzjx/hydradownload/master/release/autoupdater.py -O autoupdater.py
-wget https://raw.githubusercontent.com/zytzjx/hydradownload/master/release/cmcdeployment.py -O cmcdeployment.py
-python3 autoupdater.py
-python3 cmcdeployment.py
-# $ATHENAHOME/hydradownload -path=/opt/futuredial/hydradownloader
-# if [ $? -eq 0 ]; then
-#   echo "Success: hydradownloader."
-#   fn=$(ls /opt/futuredial/hydradownloader/)
-#   if [ ! -z $fn ]; then
-#     cd /opt/futuredial/hydradownloader
-#     unzip $fn -d $ATHENAHOME
-#     rm -f $fn
-#     cd $ATHENAHOME
-#   fi
-# fi
-crontab $ATHENAHOME/download_cron
-cp $ATHENAHOME/athena.desktop ~/Desktop/athena.desktop
-chmod +0744 ~/Desktop/athena.desktop
-gio set ~/Desktop/athena.desktop "metadata::trusted" true
-#deploy
+python3 install.py
 
-# create shortcut
-
-echo "Need reboot after finish."
