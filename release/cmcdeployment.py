1dd6b8cfc82faf651c9a0613140bd513a7040598
chencen2000 <34563412+chencen2000@users.noreply.github.com>
2021-02-04 10:42:22 -0800

    Update cmcdeployment.py

--- release/cmcdeployment.py
+++ release/cmcdeployment.py
@@ -1,54 +1,89 @@
 import os
+import json
 import redis
-import syslog
+import fcntl
 import zipfile
+import logging
+from logging.handlers import RotatingFileHandler
 
-syslog.openlog('athena.deployment')
-syslog.syslog('athena.deployment: ++ start')
+athena_home = '/opt/futuredial/athena.release'
+hydra_download_temp = '/opt/futuredial/hydradownload'
+hydradownload_lock = os.path.join(athena_home, 'hydradownloader.lck')
+athena_lock = os.path.join(athena_home, 'athena.lck')
+deployment_lock = os.path.join(athena_home, 'cmcdeployment.lck')
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s(%(lineno)d) %(message)s')
+logFile = os.path.join(athena_home, 'cmcdeployment.log')
+my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=50*1024*1024, backupCount=2, encoding=None, delay=0)
+my_handler.setFormatter(log_formatter)                                 
+my_handler.setLevel(logging.INFO)
+log = logging.getLogger('cmcdeployment')
+log.setLevel(logging.INFO)
+log.addHandler(my_handler)
+log.addHandler(logging.StreamHandler())
 
-r = redis.Redis()
-hydradownload_running = r.get('hydradownload.running')
-hydradownload_status = r.get('hydradownload.status')
-hydradownload_clientstatus = r.get('hydradownload.clientstatus')
+def deploy_frameword(rc):
+    log.info('deploy_frameword: ++')
+    ok = False
+    fw_info = None
+    x = rc.get('hd.framework.info')
+    if bool(x):
+        fw_info = json.loads(x)
+        fn = rc.get('hd.framework')
+        if os.path.exists(fn):
+            with zipfile.ZipFile(fn, 'r') as zip_ref:
+                zip_ref.extractall(athena_home)
+            os.remove(fn)
+            ok = True
+            rc.delete('hd.framework.info')
+            rc.delete('hd.framework')
+    else:
+        ok = True
+    log.info(f'deploy_frameword: -- ret={ok} info={fw_info}')
+    return ok, fw_info
 
-syslog.syslog('athena.deployment: hydradownload.running={}'.format(hydradownload_running))
-syslog.syslog('athena.deployment: hydradownload.status={}'.format(hydradownload_status))
-syslog.syslog('athena.deployment: hydradownload.clientstatus={}'.format(hydradownload_clientstatus))
+def update_client_sync(fw_info):
+    log.info(f'update_client_sync: ++ framework={fw_info}')
+    fn = os.path.join(athena_home, 'clientsync.json')
+    cs = {}
+    with open(fn) as f:
+        cs = json.load(f)
+    # update framework version
+    if bool(fw_info):
+        ver = fw_info['version'] if 'version' in fw_info else ''
+        cs['sync']['status']['framework']['version'] = ver
+    ###
+    # save clientsync.json
+    with open(fn,'w') as f:
+        json.dump(cs, f)
+    log.info('update_client_sync: --')
 
-if hydradownload_running==b'0' and hydradownload_status==b'complete':
-    syslog.syslog('athena.deployment: start deployment ...')
-    syslog.syslog('athena.deployment: set hydradownload.status=pause')
-    r.set('hydradownload.status', 'pause')
-    # keys = ['hydradownload.framework', 'hydradownload.phonedll']
-    # hydradownload.framework
-    framework_ok = True
-    syslog.syslog('athena.deployment: read key hydradownload.framework')
-    i = r.spop('hydradownload.framework')
-    while bool(i):
-        fn = i.decode('utf-8')
-        syslog.syslog('athena.deployment: value {}'.format(fn))
-        try:
-            if os.path.exists(fn):
-                with zipfile.ZipFile(fn, 'r') as f:
-                    f.extractall(os.environ['ATHENAHOME'])
-                os.remove(fn)
-        except:
-            syslog.syslog('athena.deployment: exception')
-            framework_ok = False
-        i = r.spop('hydradownload.framework')
-        pass
-    # hydradownload.phonedll
-    syslog.syslog('athena.deployment: read key hydradownload.phonedll')
-    # hydradownload.
-    syslog.syslog('athena.deployment: read key hydradownload.phonetips')
-    # save hydradownload.clientstatus
-    if framework_ok :
-        fn = os.path.join(os.environ['ATHENAHOME'], 'clientstatus.json')
-        with open(fn, 'w') as f:
-            f.write(hydradownload_clientstatus.decode('utf-8'))
+def deploy():
+    log.info('deploy: ++')
+    rc = redis.Redis(decode_responses=True)
+    fw_ok, fw_info = deploy_frameword(rc)
+    # framework ok
+    if fw_ok:
+        update_client_sync(fw_info)
+    rc.close()
+    log.info('deploy: --')    
 
-syslog.syslog('athena.deployment: delete hydradownload keys')
-for k in r.scan_iter('hydradownload*'):
-    r.delete(k)
-syslog.syslog('athena.deployment: -- complete')
-syslog.closelog()
+if __name__ == '__main__':
+    try:
+        hd_lck = open(hydradownload_lock)
+        athena_lck = open(athena_lock)
+        deploy_lck = open(deployment_lock)
+        fcntl.flock(hd_lck.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+        fcntl.flock(athena_lck.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+        fcntl.flock(deploy_lck.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except:
+        log.info('Another hydradownloader is running. Skip deployment')
+    else:
+        deploy()
+        fcntl.flock(hd_lck.fileno(), fcntl.LOCK_UN )
+        fcntl.flock(athena_lck.fileno(), fcntl.LOCK_UN)
+        fcntl.flock(deploy_lck.fileno(), fcntl.LOCK_UN)
+    finally:
+        hd_lck.close()
+        athena_lck.close()
+        deploy_lck.close()
+    
