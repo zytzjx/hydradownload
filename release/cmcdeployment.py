29235761455994d77f6b9fb1f1ccedaf44d07ce8
chencen2000 <34563412+chencen2000@users.noreply.github.com>
2021-02-05 10:45:59 -0800

    Update cmcdeployment.py

--- release/cmcdeployment.py
+++ release/cmcdeployment.py
@@ -1,9 +1,14 @@
 import os
+import sys
 import json
 import redis
 import fcntl
+import shutil
 import zipfile
 import logging
+import tempfile
+import subprocess
+import configparser
 from logging.handlers import RotatingFileHandler
 
 athena_home = '/opt/futuredial/athena.release'
@@ -20,6 +25,63 @@ log = logging.getLogger('cmcdeployment')
 log.setLevel(logging.INFO)
 log.addHandler(my_handler)
 log.addHandler(logging.StreamHandler())
+
+def deploy_deviceprofile():
+    log.info('deploy_deviceprofile: ++')
+    fn = os.path.join(athena_home, 'deploydeviceprofile.py')
+    if os.path.exists(fn):
+        p = subprocess.Popen([sys.executable, fn], cwd=athena_home)
+        p.wait()
+    else:
+        local_dp = []
+        local_dp_by_readableid = {}
+        npi_root = os.path.join(athena_home, 'NPI')
+        # easy deploy first time
+        fn = os.path.join(hydra_download_temp, 'deviceprofile.json')
+        if os.path.exists(fn):
+            with open(fn) as f:
+                dp = json.load(f)
+            # remove device profile fist
+            if 'deletelist' in dp and isinstance(dp['deletelist'], list):
+                for d in dp['deletelist']:
+                    pass
+            # add device profile
+            if 'filelist' in dp and isinstance(dp['filelist'], list):
+                for fd in dp['filelist']:
+                    fn = fd['local'] if 'local' in fd else ''
+                    if os.path.exists(fn):
+                        npi_temp = tempfile.TemporaryDirectory(suffix='dp')
+                        with zipfile.ZipFile(fn, 'r') as zip_ref:
+                            zip_ref.extractall(npi_temp.name)
+                        info_ini = configparser.ConfigParser()
+                        info_ini.read(os.path.join(npi_temp.name, 'info.ini'))
+                        folder = info_ini['information']['folder']
+                        shutil.copyfile(os.path.join(npi_temp.name, 'info.ini'), os.path.join(npi_temp.name, 'resource', folder, 'info.ini'))
+                        shutil.copytree(os.path.join(npi_temp.name, 'resource', folder), os.path.join(npi_root, folder), dirs_exist_ok=True)
+                        npi_temp.cleanup()
+                        os.remove(fn)
+                        local_dp_by_readableid[fd['readableid']] = {
+                            'folder': folder,
+                        }
+                        fd.pop('url', None)
+                        fd.pop('local', None)
+                        local_dp.append(fd)
+            os.remove(os.path.join(hydra_download_temp, 'deviceprofile.json'))
+        with open(os.path.join(npi_root, 'deviceprofile.json'), 'w') as f:
+            json.dump({
+                'syncclient': local_dp,
+                'readableid': local_dp_by_readableid
+            }, f)
+        # update clientsync.json
+        if len(local_dp) > 0:
+            with open(os.path.join(athena_home, 'clientsync.json')) as f:
+                cs = json.load(f)
+            cs['sync']['status']['deviceprofile']['filelist'].clear()
+            for dp in local_dp:
+                cs['sync']['status']['deviceprofile']['filelist'].append(dp)
+            with open(os.path.join(athena_home, 'clientsync.json'), 'w') as f:
+                json.dump(cs, f)
+    log.info('deploy_deviceprofile: --')
 
 def deploy_frameword(rc):
     log.info('deploy_frameword: ++')
@@ -62,8 +124,10 @@ def deploy():
     rc = redis.Redis(decode_responses=True)
     fw_ok, fw_info = deploy_frameword(rc)
     # framework ok
-    if fw_ok:
+    if fw_ok and bool(fw_info):
         update_client_sync(fw_info)
+    # deploy deviceprofile
+    deploy_deviceprofile()
     rc.close()
     log.info('deploy: --')    
 
